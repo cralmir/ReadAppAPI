ab9880b6ad6e25b0945e531c302912fbe586d021
Cristopher Miranda <cmiranda@rbm-corp.com>
2019-12-27 08:17:11 -0600

    Adding sequilize

--- routes/auth.js
+++ routes/auth.js
@@ -2,26 +2,38 @@ const router = require("express").Router();
 const User = require("../models/User");
 const dbConnection = require("../dbConnection/dbConnection");
 
-router.get("/users", (req, res) => {
-  User.findAll()
-    .then(user => {
-      console.log(user);
-      res.sendStatus(200);
-    })
-    .catch(err => console.log("Error: " + err));
+//Validation
+const Joi = require("@hapi/joi");
+
+const schema = Joi.object({
+  name: Joi.string()
+    .min(6)
+    .required(),
+  email: Joi.string()
+    .min(6)
+    .required()
+    .email(),
+  password: Joi.string()
+    .min(6)
+    .required()
 });
 
 router.post("/register", (req, res) => {
-  const { name, email, password, date } = req.body;
-  console.log(name);
-  User.create({
-    name: name,
-    email: email,
-    password: password,
-    date: date
-  })
-    .then(gig => res.redirect("/users"))
-    .catch(err => console.log("Error creating: " + err));
+  //Lets validate the data
+  const { error } = schema.validate(req.body);
+
+  res.send(error.details);
+
+  // const { name, email, password, date } = req.body;
+  // console.log(name);
+  // User.create({
+  //   name: name,
+  //   email: email,
+  //   password: password,
+  //   date: date
+  // })
+  //   .then(gig => res.redirect("/users"))
+  //   .catch(err => console.log("Error creating: " + err));
 });
 
 // router.post('/login',(req,res)=>{
